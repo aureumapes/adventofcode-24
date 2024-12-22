2cdba96e44068b71566754d170c700d79ff7e54f
AureumApes <janundartur@gmail.com>
2024-12-22 19:02:54 +0100

    Made first part better

--- day01/day01.go
+++ day01/day01.go
@@ -23,19 +23,15 @@ func main() {
 	}
 	sort.Ints(list1)
 	sort.Ints(list2)
-	tmpResult := make([]int, 0)
+	result := 0
 	for i, _ := range list1 {
 		i1 := list1[i]
 		i2 := list2[i]
 		if i1 > i2 {
-			tmpResult = append(tmpResult, i1-i2)
+			result += i1 - i2
 		} else {
-			tmpResult = append(tmpResult, i2-i1)
+			result += i2 - i1
 		}
-	}
-	result := 0
-	for _, r := range tmpResult {
-		result += r
 	}
 	fmt.Println("Part One: ", result)
 
